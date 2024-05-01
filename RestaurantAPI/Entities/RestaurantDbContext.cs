be4bb0c80cc7fa109169e02bcb05aa85675b02fb
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-05-01 09:24:18 +0200

    install sql server and tool to support migrations override some props in table

---
+++ RestaurantAPI/Entities/RestaurantDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantAPI;
+public class RestaurantDbContext: DbContext
+{
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Dish> Dishes { get; set; }
+
+    //możliwość dodatkowego konfigurowania pól
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Restaurant>()
+            .Property(r => r.Name)
+            .IsRequired()
+            .HasMaxLength(25);
+
+        modelBuilder.Entity<Dish>()
+            .Property(d => d.Name)
+            .IsRequired();
+    }
+}
