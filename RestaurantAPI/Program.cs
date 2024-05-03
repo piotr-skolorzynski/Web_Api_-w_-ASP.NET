c3230e1166c2471691333d9b1170d363bd330173
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-05-03 20:38:35 +0200

    create seeder

--- RestaurantAPI/Program.cs
+++ RestaurantAPI/Program.cs
@@ -1,8 +1,14 @@
 //utworzenie webhosta
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<RestaurantDbContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("RestaurantDbConnection"))); //dodanie kontekstu bazy
+builder.Services.AddScoped<RestaurantSeeder>(); //dodanie serwisu do seedowania
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -10,6 +16,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+//pobranie serwisu seeder z kontenera dependecy injection
+// musimy najpierw utworzyc scope bo serwis jest typu scope
+var scope = app.Services.CreateScope(); 
+//teraz wydobywamy konkretny obiekt czyli seeder
+var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+//odpalenie metody seed serwisu seeder
+seeder.Seed();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
---
+++ RestaurantAPI/RestaurantSeeder.cs
@@ -0,0 +1,92 @@
+﻿namespace RestaurantAPI;
+public class RestaurantSeeder
+{   
+    // prywatne pole przechowujące kontekst bazy danych
+    private readonly RestaurantDbContext _dbContext;
+    //konstruktor przez który zostanie wstrzyknięty kontekst bazy danych
+    public RestaurantSeeder(RestaurantDbContext dbContext) 
+    {
+        _dbContext = dbContext;
+    }
+    //metoda odpowiedzialan na spopulowanie bazy danych
+    public void Seed()
+    {
+        //sprawdzenie czy jesteśmy połączeni z bazą
+        if(_dbContext.Database.CanConnect())
+        {
+           //sprawdzenie czy tabela z restauracjami jest pusta
+           if(!_dbContext.Restaurants.Any())
+           {
+                //dodanie do bazy
+                var restaurants = GetRestaurants();
+                _dbContext.Restaurants.AddRange(restaurants);
+                //zapisanie zmian
+                _dbContext.SaveChanges();
+           }
+        }
+
+    }
+
+    private IEnumerable<Restaurant> GetRestaurants()
+    {
+        return new List<Restaurant>()
+        {
+            new Restaurant()
+            {
+                Name = "KFC",
+                Category = "Fast Food",
+                Description = "KFC (short for Kentucky Fried Chicken) is an American fast food restaurant chain headquartered",
+                ContactEmail = "contact@kfc.com",
+                HasDelivery = true,
+                Dishes = new List<Dish>()
+                {
+                    new Dish()
+                    {
+                        Name = "Nashville Hot Chicken",
+                        Price = 10.30M
+                    },
+
+                    new Dish()
+                    {
+                        Name = "Chicken Nuggets",
+                        Price = 5.30M
+                    }
+                },
+                Address = new Address()
+                {
+                    City = "Kraków",
+                    Street = "Długa 5",
+                    PostalCode = "30-001"
+                }
+            },
+            new Restaurant()
+            {
+                Name = "McDonald",
+                Category = "Fast Food",
+                Description = "McDonald's Corporation - an American chain of fast food restaurants, selling mainly burgers, fries and drinks",
+                ContactEmail = "contact@mcd.com",
+                HasDelivery = false,
+                Dishes = new List<Dish>()
+                {
+                    new Dish()
+                    {
+                        Name = "Cheesburger",
+                        Price = 3.5M
+                    },
+
+                    new Dish()
+                    {
+                        Name = "McDouble",
+                        Price = 6.7M
+                    }
+                },
+                Address = new Address()
+                {
+                    City = "Gdynia",
+                    Street = "Morska 33",
+                    PostalCode = "81-074"
+                }
+            }
+        };
+    }
+}
